983c7f1f77f1d142776c510f9dc876863855e65b
cuongphuong <cuongphuong.dtu@gmail.com>
2019-03-11 10:17:18 +0700

    first commit

---
+++ quanlytaisan/src/main/java/tpm/qlts/main/QuanlytaisanApplication.java
@@ -0,0 +1,13 @@
+package tpm.qlts.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlytaisanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlytaisanApplication.class, args);
+	}
+
+}
